af94a38160f23e1f7100a35834b56d537a5cf745
Web Developer <rifatmulla7@gmail.com>
2022-11-19 23:56:28 +0530

    a

--- script2.js
+++ script2.js
@@ -1,15 +1,3 @@
-    // close modal
-    function closee() {
-      document.querySelector(".modal-backdrop").style.display = "none";
-      document.querySelector(".modal").style.display = "none";    
-      document.querySelector("body").style.paddingRight= "0px";
-      document.querySelector("body").style.overflowY = "scroll";   
-  }
-
-  //selecting all required elements
-  const filterItem = document.querySelector(".wrapper2");
-  const filterImg = document.querySelectorAll(".gallery .image");
-
 // typing effect
   var TxtType = function (el, toRotate, period) {
     this.toRotate = toRotate;
@@ -53,30 +41,6 @@
   };
 
   window.onload = function () {
-
-    filterItem.onclick = (selectedItem)=>{ 
-      document.querySelector(".gallery").style.display = "flex";
-      if(selectedItem.target.classList.contains("item")){ 
-      filterItem.querySelector(".active").classList.remove("active"); 
-      selectedItem.target.classList.add("active"); 
-      let filterName = selectedItem.target.getAttribute("data-name");
-      filterImg.forEach((image) => {
-      let filterImges = image.getAttribute("data-name"); 
-          if((filterImges == filterName) || (filterName == "all")){
-              image.classList.remove("hide"); 
-              image.classList.add("show"); 
-              }else{
-              image.classList.add("hide"); 
-              image.classList.remove("show"); 
-              }
-          });
-          }
-      }
-      for (let i = 3; i < filterImg.length; i++) {
-          filterImg[i].setAttribute("onclick", "preview(this)"); 
-      }
-// ...
-
     var elements = document.getElementsByClassName('typewrite');
     for (var i = 0; i < elements.length; i++) {
       var toRotate = elements[i].getAttribute('data-type');
@@ -91,50 +55,6 @@
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
   };
-
-
-
-
-
-
-
-
-
-  //fullscreen image preview function
-  const previewBox = document.querySelector(".preview-box"),
-  categoryName = previewBox.querySelector(".title p"),
-  previewImg = previewBox.querySelector("img"),
-  closeIcon = previewBox.querySelector(".icon"),
-  shadow = document.querySelector(".shadow");
-  function preview(element){
-          
-  document.querySelector("body").style.overflow = "hidden";
-  let selectedPrevImg = element.querySelector("img").src; 
-  let selectedImgCategory = element.getAttribute("data-name"); 
-  previewImg.src = selectedPrevImg; 
-  categoryName.textContent = selectedImgCategory;
-
-  document.querySelector(".modal").style.display= "none";
-  document.querySelector(".modal-backdrop").style.display= "none";
-
-  previewBox.classList.add("show"); 
-  shadow.classList.add("show");
-
-      closeIcon.onclick = ()=>{ 
-          previewBox.classList.remove("show");
-          shadow.classList.remove("show");
-          document.querySelector("body").style.overflow = "auto";
-          document.querySelector(".modal ").style.display= "block";
-          document.querySelector(".modal-backdrop").style.display= "block";
-      }
-  }
-
-
-
-
-
-
-
 
 
   // swiper
@@ -192,5 +112,3 @@
             x.style.position = "fixed"; 
             }
         }
-
-
